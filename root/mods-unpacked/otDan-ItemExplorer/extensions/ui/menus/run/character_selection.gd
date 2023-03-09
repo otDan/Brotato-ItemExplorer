4ffb22901afd756e87e81ecc25fe4c3ab4b54a5b
otDan <otdanofficial@gmail.com>
2023-03-09 07:29:26 +0100

    Refactor character selection

--- root/mods-unpacked/otDan-ItemExplorer/extensions/ui/menus/run/character_selection.gd
+++ root/mods-unpacked/otDan-ItemExplorer/extensions/ui/menus/run/character_selection.gd
@@ -6,11 +6,11 @@ onready var ItemExplorer = get_node("/root/ModLoader/otDan-ItemExplorer/ItemExpl
 
 func _ready():
 	var selected_character = ItemExplorer.selected_character
-	print(selected_character)
-	if not selected_character == null:
-		for element in _inventory.get_children():
-			if element.item == null:
-				continue
-			if element.item.my_id == selected_character.my_id:
-				element._on_InventoryElement_pressed()
-				ItemExplorer.selected_character = null
+	if selected_character == null:
+		return
+	for element in _inventory.get_children():
+		if element.item == null:
+			continue
+		if element.item.my_id == selected_character.my_id:
+			element._on_InventoryElement_pressed()
+			ItemExplorer.selected_character = null
